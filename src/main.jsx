91c41099d1d050943e8a7423cb2844a5bb68e693
Mohammed Shamel <160044480+Mohamed-shamel1@users.noreply.github.com>
2025-09-21 21:58:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/E-commrce_ITI-Project_React">
+  <BrowserRouter >
     <App />
   </BrowserRouter>
 );
